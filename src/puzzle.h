700bf18f919cb78724ea7f6c595a8e1b1bd4d890
Matt Gallivan <gallivan.matt@gmail.com>
2021-05-30 05:31:47 -0600

    Add license headers to source files

--- src/puzzle.h
+++ src/puzzle.h
@@ -1,3 +1,8 @@
+/* Copyright (c) 2021 Matthew Gallivan <gallivan.matt@gmail.com>
+ * You may use, distribute and modify this code under the
+ * terms of the MIT license.
+ */
+
 #ifndef PUZZLE_H
 #define PUZZLE_H
 
